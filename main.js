276bc73a5e9243b8ae3b01ae2aba9b7d1db7e884
Dmytro Ivashko <ivashko.dmytro@gmail.com>
2019-11-08 14:36:51 +0200

    start

---
+++ main.js
@@ -0,0 +1,17 @@
+// const person = {
+//     name: "Dima", 
+//     secondName: "Ivashko"
+// }
+
+// sayHello.bind(person)()
+
+
+// function sayHello () {
+//     console.log(`hello i am ${this.name} ${this.secondName}`);
+// }
+
+let name = 12;
+let secondName = 12;
+
+let b = name + secondName; 
+console.log(`${b}`);
