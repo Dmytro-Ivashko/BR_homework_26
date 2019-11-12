3d91fd2390e379f3b1a6912c5318c2d5246b86ef
Dmytro Ivashko <ivashko.dmytro@gmail.com>
2019-11-12 10:22:18 +0200

    1 done

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-// const person = {
-//     name: "Dima", 
-//     secondName: "Ivashko"
-// }
+let car = {
+  manufacture: "Opel",
+  model: "Astra",
+  year: 2008,
+  speed: 120
+};
 
-// sayHello.bind(person)()
+let carInfo = () => {
+  console.log(`-- Car info --`);
+  console.log(`Manufacture: ${car.manufacture}`);
+  console.log(`Model: ${car.model}`);
+  console.log(`Year: ${car.year}`);
+  console.log(`Speed: ${car.speed}`);
+};
+let hoursSpent, minutesSpent;
+let distance = prompt("Введіть відстань яку бажаєте подолати");
 
+let distanceOvercoming = distance => {
+  let overcoming = distance / car.speed;
+  if (overcoming > 4) {
+    overcoming = overcoming + Math.floor(overcoming / 4);
+  }
+  hoursSpent = Math.floor(overcoming);
+  minutesSpent = Math.round((overcoming - hoursSpent) * 60);
+  console.log(hoursSpent);
+  console.log(minutesSpent);
+  alert(
+    `Завдяки цій машині ви зможете подолати вказану відстань приблизно за ${hoursSpent} год. ${minutesSpent} хв.`
+  );
+};
 
-// function sayHello () {
-//     console.log(`hello i am ${this.name} ${this.secondName}`);
-// }
-
-let name = 12;
-let secondName = 12;
-
-let b = name + secondName; 
-console.log(`${b}`);
+carInfo();
+distanceOvercoming(distance);
